f1513d0a7f899952db275558fe22a22ff0de9ad1
Aidan O'Brien <aidan.e.obrien@gmail.com>
2025-06-22 16:13:14 +0100

    Initial Commit

---
+++ AllMyCrap/AllMyCrapApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllMyCrapApp.swift
+//  AllMyCrap
+//
+//  Created by Aidan O'Brien on 22/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct AllMyCrapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
